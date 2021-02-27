8412aef776ba70c32be926a095525fa51ca50923
yaya <cherifi.yahia@yahoo.fr>
2021-02-27 14:35:49 +0100

    ribbon added  to the ui client.

--- clientui/src/main/java/com/clientui/clientui/proxies/MicroServiceProduitsProxy.java
+++ clientui/src/main/java/com/clientui/clientui/proxies/MicroServiceProduitsProxy.java
@@ -1,13 +1,15 @@
 package com.clientui.clientui.proxies;
 
 import com.clientui.clientui.beans.ProduitsBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "microservice-produits", url = "localhost:9001")
+@FeignClient(name = "microservice-produits")
+@RibbonClient(name = "microservice-produits")
 public interface MicroServiceProduitsProxy {
 
     @GetMapping(value = "/Produits")
